f2ec3f3fbceb844a95b3da305f4a937e4cd8bde8
Brett Nakashima <brettnak@appstorehq.com>
2011-06-16 17:43:16 -0700

    initial commit

---
+++ config/initializers/haml_initializer.rb
@@ -0,0 +1,3 @@
+# HAML + SASS initializer
+
+Sass::Plugin.add_template_location Rails.root.join( "app", "scss" )
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheCarelessLoversWebsite::Application.config.secret_token = '7d5d92770d376ced8e44b895b45edc5eb19a05e894080e1c09d2f05eefb86d6e3164e94ca813c6512661062179aabe1a102e6cfb3623154b3fa639dcf78c4d6a'
